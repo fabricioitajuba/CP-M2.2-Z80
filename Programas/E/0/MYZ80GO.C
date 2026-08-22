40d70fb1edc0c471e72c8c7933d639501f5368b2
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2026-08-22 15:41:08 -0300

    Unidade E

---
+++ Programas/E/0/MYZ80GO.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() {
+    printf("\033[2J");
+
+    printf(" CP/M 2.2 starting ...            \n\r");                                                  
+    printf("                                  \n\r");                                              
+    printf(" Loading CP/M 2.2 ...             \n\r");                                              
+    printf("           _____ _____   ____  __ \n\r");                                              
+    printf("          / ____|  __ \\ / /  \\/  |\n\r");                                              
+    printf("         | |    | |__) / /| \\  / |\n\r");                                              
+    printf("         | |    |  ___/ / | |\\/| |\n\r");                                              
+    printf("         | |____| |  / /  | |  | |\n\r");                                              
+    printf("          \\_____|_| /_/   |_|  |_|\n\r");                                              
+    printf("\n\r");                                                                                
+    printf("\n\r");                                                                                
+    printf("64K CP/M Version 2.2\n\r");  
+    printf("\n\r");
+    return;
+}
+
